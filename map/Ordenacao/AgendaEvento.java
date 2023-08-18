ddb870d70a348a3f0dc169204252f10e3171ffe6
Isaac <isaacmrodrigues2019@gmail.com>
2023-08-18 12:41:51 -0300

    InterfaceMap

--- map/Ordenacao/AgendaEvento.java
+++ map/Ordenacao/AgendaEvento.java
@@ -3,6 +3,7 @@ package Ordenacao;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class AgendaEvento {
 	
@@ -14,6 +15,26 @@ public class AgendaEvento {
 	
 	public void adicionarEvento(LocalDate data, String nome, String atracao) {
 		eventosMap.put(data, new Evento(nome, atracao));
+	}
+	
+	public void exibirAgenda() {
+		Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+		System.out.println(eventosTreeMap);
+	}
+	
+	public void obterProximoEvento() {
+		
+		LocalDate dataAtual = LocalDate.now();
+		Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+		
+		for(Map.Entry<LocalDate, Evento> entry: eventosMap.entrySet()) {
+			if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)) {
+				System.out.println("O proximo evento: " + entry.getValue() + " acontecerá na data  " + entry.getKey());
+			}
+			
+		}
+		
+			
 	}
 
 }
